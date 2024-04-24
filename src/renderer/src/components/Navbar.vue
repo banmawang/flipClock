8727c64a276ef1fce5add2df8e504ad64560845c
banmashou <agcwhyinuo@sina.com>
2024-04-24 15:39:01 +0800

    安装图片库用于显示类型的切换

--- src/renderer/src/components/Navbar.vue
+++ src/renderer/src/components/Navbar.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { AlarmClock, Time } from '@icon-park/vue-next'
 import { useConfigStore } from '@renderer/store/useConfigStore'
 
 const { config } = useConfigStore()
@@ -10,9 +11,12 @@ const toggle = () => {
 
 <template>
   <main
-    class="bg-white mb-2 w-full rounded-md px-2 opacity-100 hover:opacity-100 flex justify-between"
+    class="bg-white mb-2 w-full py-1 rounded-md px-2 opacity-100 hover:opacity-100 flex justify-between"
   >
-    <span @click="toggle">切换-- {{ config.clock.type }}</span>
+    <div @click="toggle">
+      <alarm-clock v-if="config.clock.type == 'timing'" theme="outline" size="20" fill="#333" />
+      <Time v-else theme="outline" size="20" fill="#333" />
+    </div>
     <div class="flex gap-1 text-sm opacity-80 text-gray-800">
       <router-link v-if="$route.name === 'clock'" :to="{ name: 'config' }">配置</router-link>
       <router-link v-else :to="{ name: 'clock' }">时钟</router-link>

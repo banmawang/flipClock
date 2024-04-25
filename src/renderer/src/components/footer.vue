94ba323d53ac4b31f35f63c146a64b86a754346d
banmashou <agcwhyinuo@sina.com>
2024-04-25 14:28:54 +0800

    底部文字循环滚动

--- src/renderer/src/components/footer.vue
+++ src/renderer/src/components/footer.vue
@@ -10,7 +10,7 @@ const { config } = useConfigStore()
     class="drag w-full px-2 text-center mt-2 rounded-md text-white flex justify-between items-center"
     :style="{ backgroundColor: config.footer.bgColor, color: config.footer.color }"
   >
-    <div class="text-sm">{{ config.footer.content }}</div>
+    <div class="text-sm run">{{ config.footer.content }}</div>
     <Navbar />
   </main>
 </template>
@@ -18,5 +18,21 @@ const { config } = useConfigStore()
 <style lang="scss" scoped>
 main {
   user-select: none;
+}
+
+.run {
+  animation: identifier 3s infinite both;
+}
+
+@keyframes identifier {
+  from {
+    transform: translateX(0);
+  }
+  50% {
+    transform: translateX(calc(260px - 100%));
+  }
+  to {
+    transform: translateX(0);
+  }
 }
 </style>
